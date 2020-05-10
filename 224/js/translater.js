0b765416932cf717d34069c1cf47cc0626271a59
pancoa <64609442+pancoa@users.noreply.github.com>
2020-05-10 21:52:22 +0300

    Add files via upload

---
+++ 224/js/translater.js
@@ -0,0 +1,184 @@
+var new_lang = {
+	
+
+
+  "lt0": "revolucionarno<img alt=\"\" src=\"img/@2x/p1.png\"><span>zaradi ketoze!</span>",
+  
+  "lt1": "samo danes!<span>popust 50%</span>",
+  
+  "lt2": "nova cena:<span class=\"price_main\"></span>",
+  
+  "lt3": "stara cena:<span class=\"price_old\"></span>",
+  
+  "lt4": "Do konca akcije je ostalo:",
+  
+  "lt5": "Izberite državo:<select class=\"country_select\" id=\"country\" name=\"country\"></select>",
+  
+  "lt6": "Vnesite Vaše ime:<input name=\"name\" placeholder=\"Novak Mojca\" required=\"\" type=\"text\">",
+  
+  "lt7": "Vnesite telefon:<input name=\"phone\" placeholder=\"+386-55-227-651\" required=\"\" type=\"text\">",
+  
+  "lt8": "naročiti zdaj",
+  
+  "lt9": "naročiti zdaj",
+  
+  "lt10": "Varno hujšanje<span>zaradi ketoze</span>",
+  
+  "lt11": "<b>Ketoza je proizvajanje energije iz maščob, ne pa iz glukoze, ki prihaja v organizem z ogljikovimi hidrati.</b>Če organizem neha dobivati energijo iz ogljikovih hidratov, po 7-14 dnevih nastopi stanje ketoze, kar povzroči, da se maščobe aktivno spreminjajo v energijo.",
+  
+  "lt12": "Ampak dieto, ki izključuje ogljikove hidrate iz prehrane, človek težko prenaša.<br><span style=\"color: #9f116b; font-weight: 700;\">KETO DIET vam pomaga vklopiti postopek ketoze brez da bi se mučili.</span>Kompleks zagotovi, da ketonovska <br>telesa pridejo od zunaj in pomaga vam, da se znebite odvečne maščobe.",
+  
+  "lt13": "VAŠA",
+  
+  "lt14": "KETO",
+  
+  "lt15": "DIETA",
+  
+  "lt16": "keto<span>diet</span>",
+  
+  "lt17": "Obvezna komponenta prehrane.",
+  
+  "lt18": "Kapsule sprožijo v organizmu ketozo in spremenijo nakopičeno maščobo v energijo.<b>Garantirano boste hujšali vsak dan.</b>",
+  
+  "lt19": "ZMAGOVALNA KOMBINACIJA<span class=\"big\">KETOZA+</span><span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt20": "Ponavadi potrebujete od 7 do 14 dni, da se začne ketoza. Samo 1 kapsula KETO DIET skrajša ta časovni razpon do 40-50 minut, po katerih se bo vklopil postopek transformiranja maščob v energijo in manjšanja maščobnih zalog. Jemljite<span>KETO DIET vsak dan in po enem mesecu se boste znebili do 10-15 kg odvečne teže!</span>",
+  
+  "lt21": "etapa 1.",
+  
+  "lt22": "Ketoza vam bo omogočila, da boste pokurili težko dostopno maščobo, pri čemer boste ohranili mišično maso. Zmanjšanje uživanja ogljikovih hidratov zagotavlja stabilizacijo teže in nepovrat izgubljenih kilogramov.",
+  
+  "lt23": "etapa 2.",
+  
+  "lt24": "Keto Diet vklopi ketozo: pospeši metabolizem, zmanjša apetit in neposredno napade vaše maščobne zaloge!",
+  
+  "lt25": "naročiti zdaj",
+  
+  "lt26": "<span class=\"big\">MOGOČNE KOMPONENTE</span>V SESTAVI<span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt27": "Ketoni maline",
+  
+  "lt28": "Učinkujejo podobno ketonskim telesom, ki jih proizvaja človeški organizem, so skrajno aktivni.<ul><li><span>Spremenijo</span>maščobo v energijo </li><li><span>Pospešijo</span>metabolizem</li><li><span>Ukrotijo</span>občutek lakote</li></ul>",
+  
+  "lt29": "Beta sitosterol",
+  
+  "lt30": "Nevtralizira posledice nepravilne prehrane<ul><li><span>Normalizira</span>maščobno izmenjavo </li><li><span>Očisti žile</span>od «slabega» holesterola</li><li><span>Olepša silhueto,</span>bori se s celulitem</li></ul>",
+  
+  "lt31": "Koencim Q10",
+  
+  "lt32": "Deficit koencima Q10 je vzrok za prekomerno težo v 78% primerov<ul><li><span>Normalizira</span>metabolizem</li><li><span>Zmanjšuje</span>raven slabega holesterola</li><li><span>Odstrani,</span>prenajedanje</li></ul>",
+  
+  "lt33": "Karnitin",
+  
+  "lt34": "B-vitaminom sorodna snov, ki jo sintezira človeški organizem<ul><li><span>Spremeni </span>maščobo v energijo</li><li><span>Blaži razdražljivost,</span>olepša razpoloženje</li><li><span>Odstrani</span>depresijo</li></ul>",
+  
+  "lt35": "Bromelain",
+  
+  "lt36": "Prebavljajoči beljakovine in maščobe ferment,ki ga pridobivajo iz ananasovega mesa in stebla <ul><li><span>Aktivno raztopi</span>visceralno maščobo</li><li><span>Odstrani</span>otekline</li><li><span>Preprečuje</span>zamaščevanje nontranjih organov</li></ul>",
+  
+  "lt37": "Naraven sinefrin",
+  
+  "lt38": "Aktivno raztaplja maščobe. Blokira aktivnost alfa-receptorjev maščobnih celic <ul><li><span>Bori se s «trmasto» maščobo, med drugim tudi z visceralno </span></li><li><span>Poveča porabo</span>kalorij</li><li><span>Poveča delozmožnost</span>in vzdržljivost</li></ul>",
+  
+  "lt39": "MNENJE EKSPERTOV O<span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt40": "<span>Dora Dobrovoljc</span>, slovenska dietologinja, strokovnjakinja za pravilno prehrano, v praksi že 30 let.",
+  
+  "lt41": "Pri ketozi se maščobe cepijo v maščobne kisline, ki se potem spremenijo v ketonska telesa. Čeprav se ketoza začne šele takrat, ko so zaloge glikogenov v jetrah in mišičnem tkivu popolnoma izčrpane,<b>kar se ponavadi zgodi po 7-14 dnevih.</b>",
+  
+  "lt42": "Da pospešimo ta postopek in posledično tudi kuritev maščob, jaz mojim pacientom svetujem biološki dodatek KETO DIET. KETO DIET čudovito vzdržuje organizem na keto dieti: obogati s potrebnimi elementi, ukroti občutek lakote, odstrani utrujenost in vznemirjenost.<b>KETO DIET zagotavlja zdravo in naravno hujšanje brez, tudi to je zelo važno, ohlapne kože, kar bo izredno pozitivno vplivalo na vaš organizem.</b>",
+  
+  "lt43": "Temeljne značilnosti KETO DIET sta pospešitev prehoda organizma v stanje ketoze in pomoč pri transformiranju maščob v energijo.",
+  
+  "lt44": "naročiti zdaj",
+  
+  "lt45": "Pridružite se<b>tisočem ljudi,</b>ki so že poskusili<span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt46": "Ocene",
+  
+  "lt47": "Previous",
+  
+  "lt48": "Kako malo vemo o svojem organizmu. Nikoli nisem niti pomislila, kako zapleteni procesi se v njem odvijajo, dokler nisem prišla k dietologu in tam slišala za ketozo. Lenuhinja sem. Ne znam ni biti na dietah, ni se ukvarjati s športom. Zato sem si iskala nekaj lahkega in hitrega. Za KETO DIET lahko povem le eno – zakon je! S pomočjo te diete sem shujšala za rekordne 15 kg in moja postava je zdaj idealna.",
+  
+  "lt49": "Ajša R, 34 let",
+  
+  "lt50": "«Kolikor se spomnim – vedno sem bila na dieti. Lakota, problemi s spancem in kožo so me zmeraj spremljali. V časih je bil na tehtnici minus, potem pa sem se prenajedla in znova... Za KETO DIET sem izvedela od dietologa. Izid hujšanja me je osrečil skoraj takoj. Vsak teden sem videla minus 3 kg na tehtnici. Zame je to bila resnična čarovnija. Moje samoobčutje je bilo super, lakote ni bilo, redno sem spala 7 ur».",
+  
+  "lt51": "Izabela N, 27 let",
+  
+  "lt52": "«Zame je bil KETO DIET rešitev. Nikoli prej nisem mislil, da lahko jem vse, kar imam rad, in sproti hujšam. Dejstvo je, da je bilo v moji prehrani vedno preveč ogljikovih hidratov. Kar je povzročilo prekomerno težo več kot 100 kg, ko sem bil star 29 let. Ko sem le malo skorigiral količino ogljikovih hidratov, se je začela teža manjšati. Ampak brez ogljikovih hidratov ne morem živeti, ker imam rad hitro hrano. Potem sem izvedel za KETO DIET. Ko sem začel jemati pripomoček, kuritev odvečnih maščob se je zelo pospešila, nisem se niti ogljikovim hidratom moral odpovedati. Vsega skupaj sem se v 4 mesecih znebil 25 kg. KETO DIET je neverjeten! Priporočam vsem!»",
+  
+  "lt53": "Dare K, 49 let",
+  
+  "lt54": "Kako malo vemo o svojem organizmu. Nikoli nisem niti pomislila, kako zapleteni procesi se v njem odvijajo, dokler nisem prišla k dietologu in tam slišala za ketozo. Lenuhinja sem. Ne znam ni biti na dietah, ni se ukvarjati s športom. Zato sem si iskala nekaj lahkega in hitrega. Za KETO DIET lahko povem le eno – zakon je! S pomočjo te diete sem shujšala za rekordne 15 kg in moja postava je zdaj idealna.",
+  
+  "lt55": "Ajša R, 34 let",
+  
+  "lt56": "«Kolikor se spomnim – vedno sem bila na dieti. Lakota, problemi s spancem in kožo so me zmeraj spremljali. V časih je bil na tehtnici minus, potem pa sem se prenajedla in znova... Za KETO DIET sem izvedela od dietologa. Izid hujšanja me je osrečil skoraj takoj. Vsak teden sem videla minus 3 kg na tehtnici. Zame je to bila resnična čarovnija. Moje samoobčutje je bilo super, lakote ni bilo, redno sem spala 7 ur».",
+  
+  "lt57": "Izabela N, 27 let",
+  
+  "lt58": "«Zame je bil KETO DIET rešitev. Nikoli prej nisem mislil, da lahko jem vse, kar imam rad, in sproti hujšam. Dejstvo je, da je bilo v moji prehrani vedno preveč ogljikovih hidratov. Kar je povzročilo prekomerno težo več kot 100 kg, ko sem bil star 29 let. Ko sem le malo skorigiral količino ogljikovih hidratov, se je začela teža manjšati. Ampak brez ogljikovih hidratov ne morem živeti, ker imam rad hitro hrano. Potem sem izvedel za KETO DIET. Ko sem začel jemati pripomoček, kuritev odvečnih maščob se je zelo pospešila, nisem se niti ogljikovim hidratom moral odpovedati. Vsega skupaj sem se v 4 mesecih znebil 25 kg. KETO DIET je neverjeten! Priporočam vsem!»",
+  
+  "lt59": "Dare K, 49 let",
+  
+  "lt60": "Kako malo vemo o svojem organizmu. Nikoli nisem niti pomislila, kako zapleteni procesi se v njem odvijajo, dokler nisem prišla k dietologu in tam slišala za ketozo. Lenuhinja sem. Ne znam ni biti na dietah, ni se ukvarjati s športom. Zato sem si iskala nekaj lahkega in hitrega. Za KETO DIET lahko povem le eno – zakon je! S pomočjo te diete sem shujšala za rekordne 15 kg in moja postava je zdaj idealna.",
+  
+  "lt61": "Ajša R, 34 let",
+  
+  "lt62": "Next",
+  
+  "lt63": "Čas je,<b>da se znebite kompleksov </b>",
+  
+  "lt64": "revolucionarno<img alt=\"\" src=\"img/@2x/p1.png\"><span>zaradi ketoze!</span>",
+  
+  "lt65": "samo danes!<span>popust 50%</span>",
+  
+  "lt66": "nova cena:<span class=\"price_main\"></span>",
+  
+  "lt67": "stara cena:<span class=\"price_old\"></span>",
+  
+  "lt68": "Do konca akcije je ostalo:",
+  
+  "lt69": "Izberite državo:<select class=\"country_select\" id=\"country\" name=\"country\"></select>",
+  
+  "lt70": "Vnesite Vaše ime:<input name=\"name\" placeholder=\"Novak Mojca\" required=\"\" type=\"text\">",
+  
+  "lt71": "Vnesite telefon:<input name=\"phone\" placeholder=\"+386-55-227-651\" required=\"\" type=\"text\">",
+  
+  "lt72": "naročiti zdaj",
+  
+  "lt73": "Kako delujemo",
+  
+  "lt74": "NAROČILO",
+  
+  "lt75": " Takoj po naročilu vas bo kontaktiral naš menedžer, da <br><b>ga potrdite</b>",
+  
+  "lt76": "DOSTAVA",
+  
+  "lt77": "Takoj po naročilu vas bo kontaktiral naš menedžer, da <br><b>ga potrdite</b>",
+  
+  "lt78": "PLAČILO",
+  
+  "lt79": "<b>Nobenih predplačil!</b>Plačali boste, šele ko boste dobili blago.",
+  
+  "lt80": "Garancije",
+  
+  "lt81": "Ko kupujete v naši<br>internet trgovini, ste lahko prepričani o visoki kakovosti blaga",
+  
+
+  
+
+
+	
+};
+function Translater () {
+               for (class_name in new_lang) {
+                var elements = document.getElementsByClassName(class_name);
+                if (elements.length) {
+                 for (key in elements) {
+                  elements[key].innerHTML = new_lang[class_name];
+                 }
+              }
+            }
+          };
